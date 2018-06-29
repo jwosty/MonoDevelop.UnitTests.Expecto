152d31ec07835c9b2b83b957c38399a556f5418e
jwosty <jwosty@users.noreply.github.com>
2018-06-28 21:09:48 -0600

    Successfully add a menu item

---
+++ ExpectoRunner/DateInserterCommands.cs
@@ -0,0 +1,6 @@
+﻿using System;
+namespace ExpectoRunner {
+    public enum DateInserterCommands {
+        InsertDate,
+    }
+}
---
+++ ExpectoRunner/InsertDateHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
+
+namespace ExpectoRunner {
+    public class InsertDateHandler : CommandHandler {
+        protected override void Run() {
+            Console.WriteLine(">>> Run called");
+        }
+
+        protected override void Update(CommandInfo info) {
+            Console.WriteLine(">>> Update called");
+        }
+    }
+}
