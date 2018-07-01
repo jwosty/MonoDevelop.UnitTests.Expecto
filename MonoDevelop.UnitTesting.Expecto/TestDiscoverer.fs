4decb9dafed4fa3615e6d79ada34532792d252ef
jwosty <jwosty@users.noreply.github.com>
2018-07-01 00:41:00 -0600

    Add a check

--- MonoDevelop.UnitTesting.Expecto/TestDiscoverer.fs
+++ MonoDevelop.UnitTesting.Expecto/TestDiscoverer.fs
@@ -1,12 +1,16 @@
 ﻿module MonoDevelop.UnitTesting.Expecto.TestDiscoverer
 open System
+open System.IO
 open System.Reflection
 open Expecto
 
 let getTestFromAssembly (assembly: Assembly) = Expecto.Impl.testFromAssembly assembly
 
 let getTestFromAssemblyPath (assemblyPath: string) =
-    try getTestFromAssembly (Assembly.LoadFrom assemblyPath)
+    try
+        if File.Exists assemblyPath then
+            getTestFromAssembly (Assembly.LoadFrom assemblyPath)
+        else None
     with e ->
         logfError "Exception caught while loading tests: %A" e
         None
